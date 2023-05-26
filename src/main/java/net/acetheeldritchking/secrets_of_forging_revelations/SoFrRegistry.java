82c43777522dff7c9c25fc659ceefbda02c49b4e
Smartin-b <67115074+Smartin-b@users.noreply.github.com>
2023-05-27 00:28:38 +0200

    Update SoFrRegistry.java to fix Registry

--- src/main/java/net/acetheeldritchking/secrets_of_forging_revelations/SoFrRegistry.java
+++ src/main/java/net/acetheeldritchking/secrets_of_forging_revelations/SoFrRegistry.java
@@ -5,21 +5,16 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import net.acetheeldritchking.secrets_of_forging_revelations.item.ModularPolearm;
-import se.mickelus.tetra.TetraRegistries;
+import se.mickelus.tetra.TetraMod;
 
-public class SoFrRegistry extends TetraRegistries {
-    //public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SecretsOfForgingRevelations.MOD_ID);
+public class SoFrRegistry {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TetraMod.MOD_ID);
 
-    //public static final RegistryObject<Item> ModularPolearm = ITEMS.register("modular_polearm", ModularPolearm::new);
     public static void init(IEventBus bus) {
-        bus.register(SoFrRegistry.class);
-
-        items.register(bus);
-
-        // Modular Polearm
-        items.register(ModularPolearm.identifier, ModularPolearm::new);
+        ITEMS.register(bus);
+        
+        ITEMS.register(ModularPolearm.identifier, ModularPolearm::new);
     }
 
 
