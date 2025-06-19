d27cae3ebeda5e99507b4321e74e4ed096e081d3
GamerK-2 <a01054678017@gmail.com>
2025-06-19 19:31:59 +0900

    Init for updating sofr mod into 1.20.1

--- src/main/java/net/acetheeldritchking/secrets_of_forging_revelations/SoFrRegistry.java
+++ src/main/java/net/acetheeldritchking/secrets_of_forging_revelations/SoFrRegistry.java
@@ -1,11 +1,12 @@
 package net.acetheeldritchking.secrets_of_forging_revelations;
 
+import net.acetheeldritchking.secrets_of_forging_revelations.item.ModularPolearm;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.acetheeldritchking.secrets_of_forging_revelations.item.ModularPolearm;
 import se.mickelus.tetra.TetraMod;
+
 
 public class SoFrRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TetraMod.MOD_ID);
@@ -13,8 +14,7 @@ public class SoFrRegistry {
     public static void init(IEventBus bus) {
         ITEMS.register(bus);
 
-        // Modular Polearm
+        // Modular Polearm;
         ITEMS.register(ModularPolearm.identifier, ModularPolearm::new);
     }
-
 }
